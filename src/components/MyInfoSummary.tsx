4c0e69d7fb2e604766f8b6bc41651b1ea721bdd2
jhlov <jhlovv@gmail.com>
2024-09-13 21:18:51 +0900

    로그아웃 처리

--- src/components/MyInfoSummary.tsx
+++ src/components/MyInfoSummary.tsx
@@ -1,5 +1,23 @@
+import LogoutIcon from "@mui/icons-material/Logout";
+import { getAuth } from "firebase/auth";
+import { useLogin } from "../store/useLogin";
 import "./MyInfoSummary.scss";
 
 export const MyInfoSummary = () => {
-  return <div className="my-info-summary">MyInfoSummary</div>;
+  const auth = getAuth();
+  const { isLogin } = useLogin();
+
+  return (
+    <div className="my-info-summary relative">
+      MyInfoSummary
+      {isLogin && (
+        <div
+          className="cursor-pointer absolute top-1 right-1"
+          onClick={() => auth.signOut()}
+        >
+          <LogoutIcon />
+        </div>
+      )}
+    </div>
+  );
 };
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,6 +1,20 @@
+import { Link } from "react-router-dom";
 import { MyInfoSummary } from "../components/MyInfoSummary";
+import { useLogin } from "../store/useLogin";
 
 export const Main = () => {
+  const { isLogin } = useLogin();
+  if (!isLogin) {
+    return (
+      <div>
+        <div className="p-5">
+          <div className="pb-3">로그인이 필요합니다.</div>
+          <Link to="/login">로그인 페이지로 가기</Link>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="mobile-container">
       <MyInfoSummary />
